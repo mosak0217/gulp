d70b1f39fadb7b0f86f60a1a586ea6d9b894cdb3
mosak <mosak0217@gmail.com>
2022-08-08 23:18:20 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: "production",
+  mode: process.env.NODE_ENV,
+  entry: "./src/js/main.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+rules: [
+{
+test: /\.js$/,
+use: [
+{
+loader: "babel-loader",
+options: {
+presets: [
+"@babel/preset-env",
+],
+},
+},
+],
+},
+],
+},
+target: ["web", "es5"],
+}
